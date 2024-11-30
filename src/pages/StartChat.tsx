49475495891bad32794eef63ed7d80fd3c38af23
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-11-30 21:18:48 +0800

    Added Start Chat Page

---
+++ src/pages/StartChat.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ChatHeader from '../components/chat/ChatHeader';
+import ChatWindow from '../components/chat/ChatWindow';
+import ChatInput from '../components/chat/ChatInput';
+import SuggestedFAQs from '../components/chat/SuggestedFAQs';
+
+export default function StartChat() {
+  return (
+    <div className="pt-20 min-h-screen bg-[#003025]">
+      <div className="container mx-auto px-6 py-8">
+        <div className="max-w-4xl mx-auto">
+          <ChatHeader />
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-2 space-y-6">
+              <ChatWindow />
+              <ChatInput />
+            </div>
+            <div className="lg:col-span-1">
+              <SuggestedFAQs />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
